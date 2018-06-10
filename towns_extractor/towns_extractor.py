b5ecb757355bd1b8c2c872ee405755b3d1382728
flexovich <qsfrdghty@gmail.com>
2018-06-10 16:33:10 +0300

    add remove_duplicates() fucntion

--- towns_extractor/towns_extractor.py
+++ towns_extractor/towns_extractor.py
@@ -1,5 +1,13 @@
 import csv
 
+def remove_duplicates(data):
+    preprocessed = []
+    for item in data:
+        if item not in preprocessed:
+            preprocessed.append(item)
+    return preprocessed
+
+
 with open("data/source.csv", "r", newline="",  encoding="utf-8") as source, \
      open("data/towns.csv", "w", newline="", encoding="utf-8") as output:
     reader = csv.reader(source, delimiter='\t')
@@ -11,3 +19,4 @@ with open("data/source.csv", "r", newline="",  encoding="utf-8") as source, \
 
     towns[0], towns[capital_city_index] = towns[capital_city_index], towns[0]  # setting capital city to the first
                                                                                                         # position
+    processed_towns = remove_duplicates(towns)
